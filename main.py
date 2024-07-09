6bd2ba68053b76536afbb66732e4613635cacc4d
Lewis Tough <2770287t@student.gla.ac.uk>
2024-07-09 14:15:14 +0100

    made 3d with coloured trails

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
 
 G = 6.6743e-11
 dt = 24 * 36000
-steps = 1000
+steps = 100
 colours = [
     'blue', 'red', 'green', 'orange', 'purple', 'cyan', 'magenta',
     'yellow', 'pink', 'lime', 'teal', 'coral', 'navy'
@@ -19,15 +20,16 @@ colours = [
 #distance from sun (m)
 #0, 5.79e10, 1.08e11, 1.50e11, 2.28e11, 7.78e11, 1.43e12, 2.87e12, 4.50e12
 masses = np.array([1.989e30,3.3e23, 4.87e24, 5.97e24, 6.42e23, 1.90e27, 5.68e26, 8.68e25, 1.02e26], dtype=np.float64)
-velocities = np.array([[0,0], [0, 4.787e4], [0,3.502e4], [0,2.978e4], [0,2.407e4], [0,1.307e4], [0,9.690e3], [0,6.810e3], [0,5.430e3]], dtype=np.float64)
-positions = np.array([[0,0], [5.79e10, 0], [1.08e11, 0], [1.50e11, 0], [2.28e11, 0], [7.78e11, 0], [1.43e12, 0], [2.87e12, 0],[4.50e12, 0]], dtype=np.float64)
+velocities = np.array([[0,0,0], [0, 4.787e4,0], [0,3.502e4,0], [0,2.978e4,0], [0,2.407e4,0], [0,1.307e4,0], [0,9.690e3,0], [0,6.810e3,0], [0,5.430e3,0]], dtype=np.float64)
+positions = np.array([[0,0,0], [5.79e10, 0,0], [1.08e11, 0,0], [1.50e11, 0,0], [2.28e11, 0,0], [7.78e11, 0,0], [1.43e12, 0,0], [2.87e12, 0,0],[4.50e12, 0,0]], dtype=np.float64)
 
 
 num_masses = len(masses)
-
+trail_length = 100
+trails = [np.zeros((trail_length, 3), dtype=np.float64) for _ in range(num_masses)]
 # Euler method
 def forces_calc(positions, masses):
-    forces = np.zeros_like(positions)
+    forces = np.zeros_like(positions, dtype=np.float64)
     for i in range(num_masses):
         for j in range(num_masses):
             if i != j:
@@ -47,21 +49,30 @@ def update_pos_vel(positions, velocities, masses):
 
 #plot in matplotlib
 
-fig, ax = plt.subplots()
-scat = ax.scatter(positions[:,0], positions[:,1], c=colours[:len(masses)], s=10)
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+
+scat = ax.scatter(positions[:,0], positions[:,1], positions[:,2], c=colours[:len(masses)], s=10)
+lines = [ax.plot([], [], [], color=colours[i])[0] for i in range(num_masses)]
 
 ax.set_xlim(-1e12, 1e12)
 ax.set_ylim(-1e12, 1e12)
+ax.set_zlim(-1e12, 1e12)
 ax.set_xlabel('X Pos (m)')
 ax.set_ylabel('Y Pos (m)')
+ax.set_zlabel('Z Pos (m)')
 ax.set_title('N-Body Sim')
 
 
 def update(frame):
-    global positions, velocities
+    global positions, velocities, trails
     positions, velocities = update_pos_vel(positions, velocities, masses)
-    scat.set_offsets(positions)
-    return scat,
+    scat._offsets3d = (positions[:,0],positions[:,1],positions[:,2])
+    for i in range(num_masses):
+        trails[i] = np.vstack((trails[i][1:], positions[i]))
+        lines[i].set_data(trails[i][:, 0], trails[i][:, 1])
+        lines[i].set_3d_properties(trails[i][:, 2])
+    return scat, *lines
 
-ani = animation.FuncAnimation(fig, update, frames=steps, interval=10, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=steps, interval=50, blit=False)
 plt.show()
